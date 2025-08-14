429038d50271f684dff3b8aa5da131926245694b
Juan Cuzmar <jcuzmar@protonmail.com>
2025-08-14 15:45:48 -0400

    new config

--- lua/plugins/glats.lua
+++ lua/plugins/glats.lua
@@ -1,4 +1,37 @@
 return {
+  {
+    "akinsho/bufferline.nvim",
+    keys = {
+      {
+        "L",
+        function()
+          vim.cmd("bnext " .. vim.v.count1)
+        end,
+        desc = "Next buffer",
+      },
+      {
+        "H",
+        function()
+          vim.cmd("bprev " .. vim.v.count1)
+        end,
+        desc = "Previous buffer",
+      },
+      {
+        "]b",
+        function()
+          vim.cmd("bnext " .. vim.v.count1)
+        end,
+        desc = "Next buffer",
+      },
+      {
+        "[b",
+        function()
+          vim.cmd("bprev " .. vim.v.count1)
+        end,
+        desc = "Previous buffer",
+      },
+    },
+  },
   { "navarasu/onedark.nvim" },
   {
     "folke/snacks.nvim",
